0af989370b26da40c621537e7fd5e60b427a2e23
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-23 14:22:42 -0500

    Basics - complete

---
+++ src/data/tracks.js
@@ -0,0 +1,35 @@
+import incubus from './Incubus - Loneliest.mp3';
+import incubusImg from './incubus.jpg';
+import aic from './Alice In Chains - Grind.mp3';
+import aicImg from './aic.jpg';
+import sevendust from './Sevendust - Medicated.mp3';
+import sevendustImg from './sevendust.jpg';
+import morello from './Tom Morello - Lead Poisoning.mp3';
+import morelloImg from './tommorello.jpg';
+
+export const tracks = [
+  {
+    title: 'Incubus - Loneliest',
+    src: incubus,
+    author: 'Incubus',
+    thumbnail: incubusImg,
+  },
+  {
+    title: 'Alice In Chains - Grind',
+    src: aic,
+    author: 'Alice In Chains',
+    thumbnail: aicImg,
+  },
+  {
+    title: 'Sevendust - Medicated',
+    src: sevendust,
+    author: 'Sevendust',
+    thumbnail: sevendustImg,
+  },
+  {
+    title: 'Tom Morello - Lead Poisoning',
+    src: morello,
+    author: 'Tom Morello',
+    thumbnail: morelloImg,
+  },
+]
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/index.css';
+import './styles/progress-bar.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
